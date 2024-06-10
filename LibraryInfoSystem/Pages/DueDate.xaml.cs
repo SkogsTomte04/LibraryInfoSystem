95099b8c3954f549447f2302ece1ff33b2237c84
ShockCharge <48907444+ShockCharge@users.noreply.github.com>
2024-06-10 15:31:02 +1200

    Test 4

--- LibraryInfoSystem/Pages/DueDate.xaml.cs
+++ LibraryInfoSystem/Pages/DueDate.xaml.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Windows;
+﻿using LibraryInfoSystem.Tools;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
 
-namespace LibraryInfoSystem.Tools
+namespace LibraryInfoSystem.Pages
 {
-    public class NavButton : Button
+    public partial class DueDate : Page
     {
-        public Uri NavUri { get; set; }
+        private MongoHandler mongoHandler = new MongoHandler(DataType.Duedate);
+
+        // Rename the property to reflect the MongoDB collection name
+        public ObservableCollection<DataBaseDuedate> DuedateGames { get; set; }
+
+        public DueDate()
+        {
+            InitializeComponent();
+            // Initialize the collection
+            DuedateGames = new ObservableCollection<DataBaseDuedate>();
+            DataContext = this;
+            LoadDueDateData();
+        }
+
+        private void LoadDueDateData()
+        {
+            // Retrieve due date data from MongoDB and add it to DuedateGames collection
+            DuedateGames.Clear();
+            foreach (var dueDate in mongoHandler.duedate)
+            {
+                DuedateGames.Add(dueDate);
+            }
+        }
     }
 }
