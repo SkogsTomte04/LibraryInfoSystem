9492bc3c6b35b86a60dc3c333b796e4794297cb9
William Möller <williammoller321@gmail.com>
2024-06-20 14:41:55 +1200

    Revert "Merge branch 'Jezner's-Branch'"
    This reverts commit 22a60791cd127065c98dda38ccc1bb354692ffc8, reversing
    changes made to c049dffe5196d11cbca4752a46fe1ec7ab58f956.

--- LibraryInfoSystem/Pages/DueDate.xaml.cs
+++ LibraryInfoSystem/Pages/DueDate.xaml.cs
@@ -11,8 +11,7 @@ namespace LibraryInfoSystem.Pages
     public partial class DueDate : Page
     {
         private MongoHandler mongohandler = new MongoHandler(DataType.Duedate);
-        private List<DataBaseDuedate> dueDates = new List<DataBaseDuedate>();
-        private List<DataBaseDuedate> originalDueDates = new List<DataBaseDuedate>();
+        private List<DataBaseDuedate> dueDates;
 
         public DueDate()
         {
@@ -20,17 +19,11 @@ namespace LibraryInfoSystem.Pages
             LoadDueDateData();
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService?.Navigate(new Uri("Pages/Welcome.xaml", UriKind.Relative));
-        }
-
         private void LoadDueDateData()
         {
             try
             {
                 dueDates = mongohandler.duedate;
-                originalDueDates = new List<DataBaseDuedate>(dueDates);
                 DueDateDataGrid.ItemsSource = dueDates;
             }
             catch (Exception ex)
@@ -39,22 +32,54 @@ namespace LibraryInfoSystem.Pages
             }
         }
 
-        private void SortAZ_Click(object sender, RoutedEventArgs e)
+        private void SaveChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dueDates = (List<DataBaseDuedate>)DueDateDataGrid.ItemsSource;
+
+                foreach (var dueDate in dueDates)
+                {
+                    var filter = Builders<DataBaseDuedate>.Filter.Eq(d => d.Id, dueDate.Id);
+                    var update = Builders<DataBaseDuedate>.Update
+                        .Set(d => d._title, dueDate._title)
+                        .Set(d => d._price, dueDate._price)
+                        .Set(d => d._userId, dueDate._userId)
+                        .Set(d => d._bookedDate, dueDate._bookedDate)
+                        .Set(d => d._deadlineDate, dueDate._deadlineDate)
+                        .Set(d => d._paymentMethod, dueDate._paymentMethod)
+                        .Set(d => d._isAdmin, dueDate._isAdmin);
+
+                    mongohandler.GetDuedateCollection().UpdateOne(filter, update);
+                }
+
+                MessageBox.Show("Changes saved successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while saving changes: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SortAlphabetically_Click(object sender, RoutedEventArgs e)
         {
             dueDates = dueDates.OrderBy(d => d._title).ToList();
+            DueDateDataGrid.ItemsSource = null;
             DueDateDataGrid.ItemsSource = dueDates;
         }
 
-        private void SortZA_Click(object sender, RoutedEventArgs e)
+        private void SortByLowestPrice_Click(object sender, RoutedEventArgs e)
         {
-            dueDates = dueDates.OrderByDescending(d => d._title).ToList();
+            dueDates = dueDates.OrderBy(d => d._price).ToList();
+            DueDateDataGrid.ItemsSource = null;
             DueDateDataGrid.ItemsSource = dueDates;
         }
 
-        private void DefaultSort_Click(object sender, RoutedEventArgs e)
+        private void SortByHighestPrice_Click(object sender, RoutedEventArgs e)
         {
-            // Reload the original data to reset the sorting
-            LoadDueDateData();
+            dueDates = dueDates.OrderByDescending(d => d._price).ToList();
+            DueDateDataGrid.ItemsSource = null;
+            DueDateDataGrid.ItemsSource = dueDates;
         }
     }
 }
